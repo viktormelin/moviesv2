5dbfd066106ee50971c01af2c9c93b8982edf528
dough-land <viktor.melin@me.com>
2023-02-21 18:20:03 +0100

    refactor(query): update response data

--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,18 +1,26 @@
-import type { MovieResponse } from '@/types/typings';
+import type { MovieResponse, TVResponse } from '@/types/typings';
 import axios from 'axios';
 
 export const getMoviesInTheatre = async () => {
-  return await axios.get<MovieResponse>('/api/movies/now_playing');
+  const response = await axios.get<MovieResponse>('/api/movies/now_playing');
+  response.data.type = 'movie';
+  return response;
 };
 
 export const getTrendingMovies = async () => {
-  return await axios.get<MovieResponse>('api/misc/trending?type=movie');
+  const response = await axios.get<MovieResponse>('api/misc/trending?type=movie');
+  response.data.type = 'movie';
+  return response;
 };
 
 export const getSeriesPlaying = async () => {
-  return await axios.get<MovieResponse>('/api/tv/airing_today');
+  const response = await axios.get<TVResponse>('/api/tv/airing_today');
+  response.data.type = 'tv';
+  return response;
 };
 
 export const getTrendingSeries = async () => {
-  return await axios.get<MovieResponse>('api/misc/trending?type=tv');
+  const response = await axios.get<TVResponse>('api/misc/trending?type=tv');
+  response.data.type = 'tv';
+  return response;
 };
