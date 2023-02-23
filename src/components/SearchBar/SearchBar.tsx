12ccf024a0de7f828efe7e5254061e35a5dd9296
Viktor <viktor.melin@me.com>
2023-02-23 11:44:16 +0100

    feat: add routing to preview page

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Icon from '../Icon';
 import axios from 'axios';
 import { useRouter } from 'next/router';
+
 interface LocalMedia {
   adult: boolean;
   backdrop_path: string;
@@ -30,6 +31,8 @@ interface LocalResponse {
 const SearchBar = () => {
   const router = useRouter();
 
+  const router = useRouter();
+
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [searchResult, setSearchResult] = useState<LocalResponse | undefined>(undefined);
 
@@ -45,6 +48,10 @@ const SearchBar = () => {
     if (event.key === 'Enter') {
       void submitSearch();
     }
+  };
+
+  const onClick = (item: LocalMedia) => {
+    void router.push(`/item/${item.id}?type=${item.media_type}`);
   };
 
   const onClick = (item: LocalMedia) => {
@@ -79,7 +86,20 @@ const SearchBar = () => {
         {searchResult?.results.map((item) => (
           <Box
             onClick={() => onClick(item)}
+            onClick={() => onClick(item)}
             key={item.id}
+            sx={(theme) => ({
+              'cursor': 'pointer',
+              'height': '6rem',
+              'width': '100%',
+              'padding': '0.5rem',
+              'display': 'flex',
+              'gap': '1rem',
+              'borderRadius': '0.25rem',
+              '&:hover': {
+                backgroundColor: theme.colors.dark[6],
+              },
+            })}
             sx={(theme) => ({
               'cursor': 'pointer',
               'height': '6rem',
